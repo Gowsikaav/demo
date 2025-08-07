656d8a1c33a047056cd6c58263bfa1c2c62236df
Gowsikaav <gowsikacsc@gmail.com>
2025-08-07 12:09:49 +0530

    third data

---
+++ muthayammal/src/main/java/gitconnection/string.java
@@ -0,0 +1,13 @@
+package gitconnection;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Gowsi";
+		String s2 = "Gowsi";
+		System.out.println(s1==s2);
+
+	}
+
+}
